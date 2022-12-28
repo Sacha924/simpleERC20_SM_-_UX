e12f1e1f8c63c163bf295cbaa5b0065c6fdb43ed
Sacha924 <sacha.simon@edu.devinci.fr>
2022-12-28 11:50:04 +0100

    [fix] token supply

--- src/contract/MyToken.sol
+++ src/contract/MyToken.sol
@@ -2,28 +2,29 @@
 pragma solidity ^0.8.8;
 
 contract MyToken{
-    //Var
-    string public name = "Chad Token";
-    string public symbol = "CTK";
-    uint256 public decimals = 18; //The convention is to do 18 decimal places because that's how ethers work
-    uint256 public totalSupply = 1000000000000000000;
+    // Var
+    string public name;
+    string public symbol;
+    uint256 public decimals = 18; // The convention is to do 18 decimal places because that's how ethers work
+    uint256 public totalSupply;
 
-    //mapping
+    // Mapping
     mapping(address => uint256) public balanceOf;
     mapping(address=>mapping(address=>uint256)) public allowance;
-    //events
-    event Transfer(address indexed from, address indexed to, uint value); //The erc20 standard requires us to actually log events every time a transfer events append
+
+    // Events
+    // The erc20 standard requires us to actually log events every time a transfer events append
+    event Transfer(address indexed from, address indexed to, uint value); 
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    constructor(string memory _name,string memory _symbol,uint256 _decimals,uint256 _totalSupply){
+    constructor(string memory _name,string memory _symbol,uint256 initialSupply){
         name=_name;
         symbol=_symbol;
-        decimals=_decimals;
-        totalSupply=_totalSupply;
-        balanceOf[msg.sender] = totalSupply; //the person who deploys the contrat gets all the token
+        totalSupply=initialSupply* 10**uint256(decimals); // Warning, don't forget to update total supply with the decimal amount
+        balanceOf[msg.sender] = totalSupply; // The person who deploys the contrat gets all the token
     }
 
-    //transfer amount of tokens to an address
+    // Transfer amount of tokens to an address
     function tranfer(address _to, uint256 _value) external returns(bool success){
         require(balanceOf[msg.sender] >= _value, "Not enough money");
         require(_value<totalSupply, "You asked for too much tokens");
@@ -46,7 +47,7 @@ contract MyToken{
         return true;
     }
 
-    //internal helper transfer function with required safety checks
+    // Internal helper transfer function with required safety checks
     function _transfer(address _from, address _to, uint256 _value) internal {
         require(_to != address(0)); // Ensure sending is to valid address! 0x0 address can be used to burn() 
         balanceOf[_from] = balanceOf[_from] - _value;
